f0c265b28e35e512ed1de7ee8293dc0e2fd9d9d9
Jonathan Will <jwill@live.com>
2016-03-24 11:29:49 -0700

    Initial Commit

---
+++ AVVSO/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AVVSO
+//
+//  Created by Will, Jonathan R on 3/24/16.
+//  Copyright © 2016 jwill. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
